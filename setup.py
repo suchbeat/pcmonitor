808367516042a5297a1568d6b05638a46d5a6a3d
Yuriy Stetskiv <ystetskiv@lohika.com>
2016-12-01 17:06:06 +0200

    setup.py fix

--- setup.py
+++ setup.py
@@ -1,18 +1,11 @@
 from distutils.core import setup
 
 setup(
-    name = 'pcmonitor',
-    version = '0.0.1',
-    description = 'HTOP Python package',
-    author = 'suchbeat',
-    author_email = 's.u.chbeat@gmail.com',
-    url = 'https://github.com/zaiste/zaiste-py',
+    name='pcmonitor',
+    version='0.0.1',
+    description='HTOP Python package',
+    author='suchbeat',
+    author_email='s.u.chbeat@gmail.com',
+    url='https://github.com/suchbeat/pcmonitor.git',
     py_modules=['pcmonitor'],
-    install_requires=[
-        # list of this package dependencies
-    ],
-    entry_points='''
-        [console_scripts]
-        zaiste=zaiste:cli
-    ''',
 )
